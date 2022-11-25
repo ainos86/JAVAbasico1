fe79e9f2f3be6374876cb5a356fffef43e6cfac7
ainos86 <116199341+ainos86@users.noreply.github.com>
2022-11-25 13:27:21 +0100

    first commit

---
+++ src/JAVAbasico1/Ejercicio2.java
@@ -0,0 +1,18 @@
+package JAVAbasico1;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        double productPrice = 24.99;
+        double totalPrice = calculateTotalPrice(productPrice);
+
+        System.out.println("El precio total es de " + totalPrice + " " + "euros.");
+    }
+
+    public static double calculateTotalPrice (double productPrice){
+        double iva = productPrice * 0.21;
+        return productPrice + iva;
+
+    }
+}
+
